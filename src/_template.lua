3976b051d0d9fc1e473272e2c7a0c1379219e2d8
William Brawner <me@wbrawner.com>
2023-12-16 22:00:54 -0700

    Add lua template file

---
+++ src/_template.lua
@@ -0,0 +1,16 @@
+require "util"
+
+function part1(input)
+    return #input
+end
+
+function part2(input)
+    return #input
+end
+
+local test_input = read_test_input()
+assert_equals(part1(test_input), 0)
+local input = read_input()
+print(part1(input))
+assert_equals(part2(test_input), 0)
+print(part2(input))
